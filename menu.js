f48304abfdbad425874f31fa0091e3de1aa56175
Jeremiah Alaba Juba <65506247+Juba-dev@users.noreply.github.com>
2025-07-11 11:25:35 +0100

    Add files via upload

---
+++ menu.js
@@ -0,0 +1,10 @@
+function menu(){
+    let menulist = document.getElementById("menulist");
+
+    menulist.style="left: 0px; transition: all 2s;"
+}
+function hide(){
+    let hidemenu = document.getElementById("menulist");
+
+    hidemenu.style="right: 750px;";
+}
